ac44552b0f3394228a9cb1d73329852bd5e8aecc
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-10-20 14:32:35 +0530

    Add files via upload

---
+++ Module3/20-10-sqflite/add.dart
@@ -0,0 +1,83 @@
+//@dart=2.9
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'mydb.dart';
+
+class AddStudent extends StatefulWidget
+{
+
+  @override
+  AddState createState() => AddState();
+
+
+}
+
+class AddState extends State<AddStudent>
+{
+
+  TextEditingController name = TextEditingController();
+  TextEditingController rollno = TextEditingController();
+  TextEditingController address = TextEditingController();
+
+  MyDb mydb = new MyDb();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+   // super.initState();
+
+    mydb.open();
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Add Student"),
+        ),
+        body:Container(
+          padding: EdgeInsets.all(30),
+          child: Column(children: [
+            TextField(
+              controller: name,
+              decoration: InputDecoration(
+                hintText: "Stuent Name",
+              ),
+            ),
+
+            TextField(
+              controller: rollno,
+              decoration: InputDecoration(
+                hintText: "Roll No.",
+              ),
+            ),
+
+            TextField(
+              controller: address,
+              decoration: InputDecoration(
+                hintText: "Address:",
+              ),
+            ),
+
+            ElevatedButton(onPressed: ()
+            {
+              print('clicked');
+              mydb.db.rawInsert("INSERT INTO students (name, roll_no, address) VALUES (?, ?, ?);",
+                  [name.text, rollno.text, address.text]);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("New Student Added")));
+
+              name.text = "";
+              rollno.text = "";
+              address.text = "";
+
+            }, child: Text("Save Student Data")),
+          ],),
+        )
+    );
+
+  }
+
+}
