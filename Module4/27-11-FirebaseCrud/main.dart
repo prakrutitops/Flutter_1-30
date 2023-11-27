1ba2eb1f5554fca161e6d0f915623bec886ebd00
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2023-11-27 14:36:09 +0530

    Add files via upload

---
+++ Module4/27-11-FirebaseCrud/main.dart
@@ -0,0 +1,52 @@
+//{
+//   "rules": {
+//     ".read": true,
+//     ".write": true
+//   }
+// }
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'add.dart';
+
+
+void main()
+{
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+
+
+    final Future<FirebaseApp> _initailization = Firebase.initializeApp();
+    return FutureBuilder(
+        future: _initailization,
+        builder: (context, snapshot) {
+          //Something from Firebase
+          if (snapshot.hasError) {
+            print('Something Went Wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                appBarTheme: const AppBarTheme(
+                  centerTitle: true,
+                ),
+              ),
+              home: const AddPage(),
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        });
+  }
+}
+
