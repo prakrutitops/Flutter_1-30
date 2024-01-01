fe7d70f2afdd992ff064b00d89ae7957c955a267
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-01-01 10:50:17 +0530

    Add files via upload

---
+++ 1-1-Project/main.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:project123/user/login/login.dart';
+import 'package:project123/user/splashscreen/splashscreen.dart';
+
+
+
+void main()
+{
+  //Launch FirstScreen - SplashScreen
+  runApp(MaterialApp(home:MyApp()));
+}
+
---
+++ 1-1-Project/user/bottom_navigation/download/download.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+import 'dart:io';
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+import '../../../constants.dart';
+
+class Download extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return DownloadPage();
+  }
+}
+
+class DownloadPage extends State<Download> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+    return Scaffold(
+
+    );
+  }
+}
