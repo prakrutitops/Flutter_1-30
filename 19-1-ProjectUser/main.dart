18adecfac9946882e718d0ea2ce348d97ea5096e
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2024-01-19 10:30:28 +0530

    Add files via upload

---
+++ 19-1-ProjectUser/main.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:project123/user/login/login.dart';
+import 'package:project123/user/splash_screen/splash_screen.dart';
+
+
+
+void main()
+{
+  //Launch FirstScreen - SplashScreen
+  runApp(MaterialApp(home:SplashScreen()));
+}
+
